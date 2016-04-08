3c410ef07b4d1296112f61ad8cd426f0a2a9339b
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-04-08 09:46:43 -0700

    add updates to index.html and add content on the starting of the business and user interface logic to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
+//Business Logic
+function Pizza (size, topping, extraToppings){
+  this.sizeKey = size;
+  this.toppingKey = topping;
+  this.extraToppingsKey = extraTopping;
+}
 
+//User Interface Logic
+$(document).ready(function() {
+  $("form#pizza").submit(function(event)
+  event.preventDefault();
+
+  var sizeInput = $("input:radio[name=pizza-size]:checked").val();
+  var toppingInput = $("input:radio[name=toppings]:checked").val();
+  var extraToppingsInput = $("input:radio[name=extraToppings]:checked").val();
+  var quantityInput = $("#quantity").val();
+
+
+
+
+
+});
+});
