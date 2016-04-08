c90fbdf87289763fbf616791ec14c22c71db48ea
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-04-08 16:42:48 -0700

    add business and user logic content with creation of abjects and prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,45 @@
 //BUSINESS LOGIC
-function Pizza (size, flavor, toppings){
-  this.size = size;
+function Pizza (flavor, size, pizzaQuantity, pizzaPrice, toppingsQuantity, toppingsPrice){
   this.flavor = flavor;
-  this.toppings = toppings;
+  this.size = size;
+  this.pizzaQuantity = pizzaQuantity;
+  this.pizzaPrice = pizzaPrice
+  this.toppingsQuantity = toppingsQuantity;
+  this.toppinsPrice = toppingsPrice
+
 }
+console.log(Pizza);
 
 Pizza.prototype.sizePrice = function() {
   if (this.size === "small"){
-    return 15;
+    this.pizzaPrice = 15;
   } else if (this.size === "medium") {
-    return 20;
+    this.pizzaPrice = 20;
   } else {
-    return 25; /*large pizza price*/
+    return this.pizzaPrice = 25; /*large pizza price*/
   }
 }
+console.log(Pizza.prototype.sizePrice);
 
-Pizza.prototype.pizzaPrice = function() {
-  var sizePrice = this.sizePrice(this.size);
-              console.log(sizePrice);
-  var totalPrice = sizePrice + (this.toppings * 2.00);
-  return totalPrice;
+Pizza.prototype.toppingPrice = function() {
+  if (this.toppingsQuantity === "topping-total"){
+    this.toppinsPrice = 2;
+  } else if (this.toppingsQuantity !== "topping-total"){
+    this.toppingsPrice = 0;
+  } else {
+    return null;
+  }
+console.log(Pizza.prototype.toppingPrice);
 }
+
+Pizza.prototype.sizePricePlusToppingsPrice = function() {
+  var sizePrice = this.sizePrice(this.size);
+  var toppingPrice = this.toppingPrice(thins.toppingsQuantity);
+  var totalPrice = sizePrice + toppingPrice;
+
+  console.log(totalPrice);
+}
+
 
 //USER INTERFACE LOGIC
 $(document).ready(function() {
@@ -31,19 +50,27 @@ $(document).ready(function() {
   var doughInput = $("input:radio[name=pizza-dough]:checked").val();
   var flavorInput = $("input:radio[name=pizza-flavor]:checked").val();
   var extraToppingsInput = $("input:radio[name=extraToppings]:checked").val();
-  var quantityInput = $("#pizza-quantity").val();
+  var toppingsQuantityInput = parseInt($("#toppings-quantity").val());
+  console.log(toppingsQuantityInput);
+  var pizzaQuantityInput = parseInt($("#pizza-quantity").val());
+  console.log(pizzaQuantityInput);
+  var pizzaPrice = [];
 
 
-  var newPizza = new Pizza(sizeInput, flavorInput, extraToppingsInput);
+  var newPizza = new Pizza( flavorInput, sizeInput, pizzaQuantityInput,  Pizza.prototype.sizePrice, toppingsQuantityInput, Pizza.prototype.toppingPrice);
+
   console.log(newPizza);
 
-  var OrderPrice = newPizza.pizzaPrice() * quantityInput;
+  var OrderPrice = (newPizza.sizePrice() * pizzaQuantityInput) + (newPizza.toppingPrice() * toppingsQuantityInput);
 
                     console.log(OrderPrice);
 
-
-
-
+  $(".orderSummary").show();
+  $(".pizza-flavor").text(newPizza.flavor);
+  $(".pizza-size").text(newPizza.size);
+  $(".pizza-extra-toppings").text(extraToppingsInput);
+  $(".pizza-quantity").text(pizzaQuantityInput);
+  $(".pizza-price").text("$" + OrderPrice);
 
 });
 });
