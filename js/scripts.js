47abeb5619bb3438e956b46c7ea31cb22a46d182
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-04-08 13:16:52 -0700

    add final completed, but not yet tested, business logic to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
-//Business Logic
-function Pizza (size, topping, extraToppings){
-  this.sizeKey = size;
-  this.toppingKey = topping;
-  this.extraToppingsKey = extraTopping;
+//BUSINESS LOGIC
+function Pizza (size, flavor, toppings){
+  this.size = size;
+  this.flavor = flavor;
+  this.toppings = toppings;
 }
 
-//User Interface Logic
+Pizza.prototype.sizePrice = function() {
+  if (this.size === "small"){
+    return 15;
+  } else if (this.size === "medium") {
+    return 20;
+  } else {
+    return 25; /*large pizza price*/
+  }
+}
+
+Pizza.prototype.pizzaPrice = function() {
+  var sizePrice = this.sizePrice(this.size);
+              console.log(sizePrice);
+  var totalPrice = sizePrice + (this.toppings * 2.00);
+  return totalPrice;
+}
+
+//USER INTERFACE LOGIC
 $(document).ready(function() {
-  $("form#pizza").submit(function(event)
+  $("form#pizza").submit(function(event){
   event.preventDefault();
 
   var sizeInput = $("input:radio[name=pizza-size]:checked").val();
-  var toppingInput = $("input:radio[name=toppings]:checked").val();
+  var doughInput = $("input:radio[name=pizza-dough]:checked").val();
+  var flavorInput = $("input:radio[name=pizza-flavor]:checked").val();
   var extraToppingsInput = $("input:radio[name=extraToppings]:checked").val();
-  var quantityInput = $("#quantity").val();
+  var quantityInput = $("#pizza-quantity").val();
+
+
+  var newPizza = new Pizza(sizeInput, flavorInput, extraToppingsInput);
+  console.log(newPizza);
+
+  var OrderPrice = newPizza.pizzaPrice() * quantityInput;
+
+                    console.log(OrderPrice);
 
 
 
