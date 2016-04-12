d1d019ac633187ea3f238f762b8889e3c2cddefb
Fernanda Lowe <fe_lowe@hotmail.com>
2016-04-11 17:09:07 -0700

    pizza april 11th

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+//BUSINESS LOGIC
+function Pizza (flavor, size, toppings){
+  this.flavor = flavor;
+  this.size = size;
+  this.toppings = toppings;
+  this.pizzaPrice = 0;
+  this.toppingPrice = 0;
+  this.total = 0;
+}
+
+Pizza.prototype.sizePrice = function() {
+  if (this.size === "small"){
+    this.pizzaPrice = 15;
+  } else if (this.size === "medium") {
+    this.pizzaPrice = 20;
+  } else {
+    this.pizzaPrice = 25; /*large pizza price*/
+  }
+}
+
+Pizza.prototype.toppingsPrice = function(){
+  this.toppingPrice = (this.toppings * 2);
+}
+
+//USER INTERFACE LOGIC
+$(document).ready(function() {
+  $("form#pizza").submit(function(event){
+  event.preventDefault();
+
+  var sizeInput = $("input:radio[name=pizza-size]:checked").val();
+  var doughInput = $("input:radio[name=pizza-dough]:checked").val();
+  var flavorInput = $("input:radio[name=pizza-flavor]:checked").val();
+  var extraToppingInput = $("input:radio[name=extraToppings]:checked").val();
+  var toppingQuantityInput = parseInt($("#toppings-quantity").val());
+  var pizzaQuantityInput = parseInt($("#pizza-quantity").val());
+
+  var newPizza = new Pizza(flavorInput, sizeInput, toppingQuantityInput);
+newPizza.sizePrice();
+newPizza.toppingsPrice();
+console.log(newPizza);
+console.log(newPizza.toppingPrice);
+newPizza.total = newPizza.toppingPrice + newPizza.pizzaPrice;
+console.log(newPizza.total);
+  $(".orderSummary").show();
+  $(".pizza-flavor").text(newPizza.flavor);
+  $(".pizza-size").text(newPizza.size);
+  $(".pizza-extra-toppings").text(extraToppingInput);
+  $(".pizza-quantity").text(pizzaQuantityInput);
+  $(".pizza-price").text("$" + newPizza.total);
+
+});
+});
---
+++ readme.md
@@ -0,0 +1,34 @@
+<h1>Week 4: JavaScript Object Oriented(OO) independent project</h1>
+
+<h2>Project name: Pizza Order, 04/08/2016</h2>
+
+<h3>By <strong><i>Fernanda Lowe</i></strong></h3>
+
+<h2>Description</h2>
+
+<p>This projects requires the creation of a website for pizza ordering where an user can choose the pizza size, a topping, as well extra individual toppings, and after placing the order, see the final cost. All based on knowledge acquired in week 1 to 4 of the Intro Program at the <a href="https://www.learnhowtoprogram.com/intro-to-programming"> <strong>Epicodus Programing Bootcamp.</strong></a>
+
+<h2>Setup/Installation Requirements</h2>
+<ol>
+  <li>Access the website:</li>
+  <li>Surf through the page</li>
+  <li>Use the navigation bar to access the desired content</li>
+</ol>
+
+<h2>Known Bugs</h2>
+
+<p>Unknown bugs</p>
+
+<h2>Support and contact details</h2>
+
+  <p>If you find any issue with this application please email <a href="mailto:felowe@felowe.com">Fernanda Lowe</a></p>
+
+<h2>Technologies Used</h2>
+
+  <p>Languages and tools used to create this app: <b>HTML</b>, <b>CSS</b>, <b>Bootstrap</b> and <b>jQuery</b>.</p>
+
+<h2>License</h2>
+
+  <p>Application can be used under licenses MIT and Apache.</p>
+
+<p>Copyright&copy 2016 Fernanda Lowe, <a href="http://www.epicodus.com">Epicodus.</a></p>
